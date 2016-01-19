91eaad704620df706145e0b247e2ad4a41f92756
Jelle Wiersma <Jelle Wiersma>
2016-01-19 14:26:24 +0100

    camera pagina en lampbedienings pagina toegevoegd

---
+++ Server/public_html/camera.php
@@ -0,0 +1,26 @@
+<?php require_once "../header.php"; ?>
+<br>
+
+    <h1>Camerabediening</h1>
+    <hr>
+<br>
+<br>
+
+<video width="100%" height="100%" controls poster="/test video & audio/video/Snowball.png"  >
+    <source src="/test video & audio/video/Snowball.mp4" type="video/mp4" />
+    <source src="/test video & audio/video/Snowball.ogv" type="video/ogg" />
+    <source src="/test video & audio/video/Snowball.wbem" type="video/webm" />
+    <em>Sorry, your browser doesn't support HTML5 video.</em>
+</video>
+
+<p>
+
+    <button>AAN</button>
+    <button>UIT</button>
+
+</p>
+
+
+
+
+<?php require_once "../footer.php"; ?>
