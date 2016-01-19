a5b7117f3f92a4cfd43b0cb8dfaaaec9a6382b2d
Jonathan Peeman <jonathan.peeman@student.hu.nl>
2016-01-19 10:00:00 +0100

    Start navigation bar (html + css)
    Signed-off-by: Jonathan Peeman <jonathan.peeman@student.hu.nl>

---
+++ Server/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/domoit.css" rel="stylesheet">
+
+    <title>DomoIT</title>
+</head>
+<body>
+
+<div id="navigation">
+    <div class="back-button active">&larr;</div>
+    <div class="name">DomoIT</div>
+    <div class="settings-button"><img src="/assets/images/settings-icon.png"></div>
+</div>
+
+<div id="content">
---
+++ Server/public_html/index.php
@@ -0,0 +1,5 @@
+<?php require_once "../header.php"; ?>
+
+Content
+
+<?php require_once "../footer.php"; ?>
