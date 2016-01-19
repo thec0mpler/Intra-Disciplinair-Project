760e635161651b6825833fd9bebbd15404502613
daphne hogeterp <daphne hogeterp>
2016-01-19 15:07:36 +0100

    Signed-off-by: daphne hogeterp <daphne hogeterp>

--- Server/public_html/index.php
+++ Server/public_html/index.php
@@ -8,15 +8,15 @@
 <h3>Hoofdmenu</h3>
 
     <p>Lamp
-<button> <img src="plaatje_lamp.jpg" alt="Lamp" style="width:100%;height:100%;">
+<button> <img src="/assets/images/plaatje_lamp.jpg" alt="Lamp" style="width:100%;height:100%;">
 </button> </p>
 
     <p>Camera
-<button> <img src="plaatje_camera.jpg" alt="Camera" style="width:100%;height:100%;">
+<button> <img src="/assets/images/plaatje_camera.jpg" alt="Camera" style="width:100%;height:100%;">
 </button> </p>
 
     <p>Hulpoproep
-<button> <img src="plaatje_hulp.jpg" alt="Hulpknop" style="width:100%;height:100%;">
+<button> <img src="/assets/images/plaatje_hulp.jpg" alt="Hulpknop" style="width:100%;height:100%;">
 </button> </p>
 
 
