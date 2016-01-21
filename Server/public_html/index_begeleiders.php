8b0fa44563e6b4188bf6f139579b1df573dd9552
daphne hogeterp <daphne hogeterp>
2016-01-21 14:18:59 +0100

    Signed-off-by: daphne hogeterp <daphne hogeterp>

---
+++ Server/public_html/index_begeleiders.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/domoit.css" rel="stylesheet">
+
+    <title>DomoIT</title>
+</head>
+<body>
+
+<div id="navigation">
+    <a href="index_begeleiders.php" class="back-button active">
+        <img src="/assets/images/house-icon.png">
+    </a>
+    <div class="name">DomoIT</div>
+    <a href="opties_begeleiders.php" class="settings-button">
+        <img src="/assets/images/settings-icon.png">
+    </a>
+</div>
+
+<div id="content">
+    <div class="clearfix"></div>
+
+
+
+<button onclick="location.href='woningselectie.php'" >Selecteer woning</button><br>
+<button onclick="location.href='meldingen.php'" >Hulpoproepen</button><br>
+
+
+<!-- <FORM class="begeleiders" METHOD="LINK" ACTION="woningselectie.php">
+<INPUT TYPE="submit" VALUE="Selecteer woning">
+</FORM>
+<button type="buttons">Meldingen</button> -->
+
+
+</div>
+
+</body>
+</html>
+
---
+++ Server/public_html/woning_1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/domoit.css" rel="stylesheet">
+
+    <title>DomoIT</title>
+</head>
+<body>
+
+<div id="navigation">
+    <a href="index_begeleiders.php" class="back-button active">
+        <img src="/assets/images/house-icon.png">
+    </a>
+    <div class="name">DomoIT</div>
+    <a href="opties_begeleiders.php" class="settings-button">
+        <img src="/assets/images/settings-icon.png">
+    </a>
+</div>
+
+<div id="content">
+    <div class="clearfix"></div>
+
+
+Selecteer de woning die u wilt bekijken
+
+<div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="woning_1.php">Woning 1</a></li>
+    <li><a href="woning_2.php">Woning 2</a></li>
+    <li><a href="woning_3.php">Woning 3</a></li>
+  </ul>
+</div>
+
+
+
+
+</div>
+
+</body>
+</html>
+
