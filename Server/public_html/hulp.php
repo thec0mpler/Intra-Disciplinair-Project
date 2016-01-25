d3a5c87ffbf34ba7ea3cc5f916453f82d2d1ffb2
daphne hogeterp <daphne hogeterp>
2016-01-25 13:22:37 +0100

    alert box
    Signed-off-by: daphne hogeterp <daphne hogeterp>

--- Server/public_html/hulp.php
+++ Server/public_html/hulp.php
@@ -1,12 +1,16 @@
 <?php require_once "../header.php"; ?>
 
-
+<script type="text/javascript">
+function hulpoproep() {
+    return alert('Uw hulpoproep is verzonden');
+}
+</script>
 
 <br>
 <p>
 Hulp nodig?
 </p>
-<button class="hulpbutton" type="button" onclick="alert('Uw hulpoproep is verzonden')"> Ja </button>
+<button class="hulpbutton" type="button" onclick=hulpoproep()> Ja </button>
 <button class="hulpbutton" type="button" onclick="location.href='index.php'">Nee</button>
 
 
