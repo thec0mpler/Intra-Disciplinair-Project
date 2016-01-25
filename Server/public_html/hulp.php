6cb812969b6e250e71dea3d56c21639049a4da16
daphne hogeterp <daphne hogeterp>
2016-01-25 13:14:01 +0100

    database
    Signed-off-by: daphne hogeterp <daphne hogeterp>

--- Server/public_html/hulp.php
+++ Server/public_html/hulp.php
@@ -1,10 +1,12 @@
 <?php require_once "../header.php"; ?>
 
+
+
 <br>
 <p>
 Hulp nodig?
 </p>
-<button class="hulpbutton" type="button">Ja</button>
+<button class="hulpbutton" type="button" onclick="alert('Uw hulpoproep is verzonden')"> Ja </button>
 <button class="hulpbutton" type="button" onclick="location.href='index.php'">Nee</button>
 
 
